26138a3335d413515178cb4f81cf048555fba254
Sonat Saygın İPEK <sonatipek55@gmail.com>
2023-09-25 16:11:47 +0300

    created the basic structure of the API application

---
+++ app.js
@@ -0,0 +1,19 @@
+// Node Modules
+const express = require('express');
+
+// Instance
+const app = express();
+
+// Variables
+const PORT = 3000;
+
+// Routes
+// HTTP Methods: GET, POST, PUT, DELETE
+app.get("/", (req, res) => {
+    res.send("Home Page");
+})
+
+// Listen
+app.listen(PORT, _ => {
+    console.log("http://localhost:"+PORT);
+})
