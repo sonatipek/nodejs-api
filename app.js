6582539f7616fc6ac49770a467277a126f4c9379
Sonat Saygın İPEK <sonatipek55@gmail.com>
2023-09-28 15:21:51 +0300

    cors npm package for the CORS error

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // Node Modules
 const express = require('express');
+const cors = require('cors');
 
 // Custom Modules
 const productRoutes = require('./routes/products');
@@ -10,14 +11,19 @@ const app = express();
 
 // Middlewares
 app.use(express.json()) //Read json objects
+app.use(cors({
+    origin: "*", //["abc.com", "b.com"]
+    methods: ["GET"] //["GET", "POST"]
+}));
 
 // !You can use this code part for CORS or you can use 'cors' npm package
-// app.use((req, res, next) => {
-//     res.setHeader("Access-Control-Allow-Origin", "*"); //? a.com, b.com or *(asterisk) = all
-//     res.setHeader("Acces-Control-Allow-Methods", "*"); //? POST, GET etc. or *(asterisk) = All
+/* app.use((req, res, next) => {
+     res.setHeader("Access-Control-Allow-Origin", "a.com b.com"); //? a.com, b.com or *(asterisk) = all
+     res.setHeader("Acces-Control-Allow-Methods", "POST GET"); //? POST, GET etc. or *(asterisk) = All
 
-//     next();
-// });
+     next();
+ });
+ */
 
 // Variables
 const PORT = 3000;
