8832f93aff59861659d263b12510783fc21775b0
Sonat Saygın İPEK <sonatipek55@gmail.com>
2023-09-28 15:11:31 +0300

    a middleware has been written for the CORS  error

--- app.js
+++ app.js
@@ -11,6 +11,14 @@ const app = express();
 // Middlewares
 app.use(express.json()) //Read json objects
 
+// !You can use this code part for CORS or you can use 'cors' npm package
+// app.use((req, res, next) => {
+//     res.setHeader("Access-Control-Allow-Origin", "*"); //? a.com, b.com or *(asterisk) = all
+//     res.setHeader("Acces-Control-Allow-Methods", "*"); //? POST, GET etc. or *(asterisk) = All
+
+//     next();
+// });
+
 // Variables
 const PORT = 3000;
 
